dceafde6f4a34b5638df224cc23fc8663d6d4f04
ppedziwiatr <peter@redstone.finance>
2021-09-04 13:33:52 +0200

    feat: example webapp with the new SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {SmartWeaveWebFactory} from "redstone-smartweave";
+import Arweave from "arweave";
+
+const arweave = Arweave.init({
+  host: 'dh48zl0solow5.cloudfront.net', // Hostname or IP address for a Arweave host
+  port: 443, // Port
+  protocol: 'https', // Network protocol http or https
+  timeout: 60000, // Network request timeouts in milliseconds
+  logging: false // Enable network request logging
+});
+
+const smartweave = SmartWeaveWebFactory.memCached(arweave);
+
+const providersRegistryContract = smartweave.contract(
+  "OrO8n453N6bx921wtsEs-0OCImBLCItNU5oSbFKlFuU"
+);
 
 function App() {
+
+  const [contractState, setContractState] = useState({});
+
+  useEffect(() => {
+    async function fetchContractData() {
+      const state = await providersRegistryContract.readState();
+      setContractState(state);
+    }
+
+    fetchContractData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +46,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          {JSON.stringify(contractState)}
+        </div>
       </header>
     </div>
   );
